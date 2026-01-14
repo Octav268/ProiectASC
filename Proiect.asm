2e68dbd1ef6de10ff94af75515ac878b2e90ce18
Tudorica01 <tudorica0108@gmail.com>
2026-01-14 14:07:17 +0200

    Add files via upload

--- Proiect.asm
+++ Proiect.asm
@@ -60,7 +60,7 @@ conversie_loop:
     or al, bl           
     stosb
     loop conversie_loop
-
+;----------------------------------------------------------------------------------------------------------------------
     ;   cuvantul C: bitii 0-3: XOR, 4-7: OR, 8-15: suma modulo 256 
     lea si, valori
     mov cl, nr_octeti
@@ -84,10 +84,63 @@ sort_j:
     jae no_swap       ; pentru egalitate nu trebuie modificat
     
     mov [si], bl    
-    mov [si+1], al  
+    mov [si+1], al    
 
+no_swap:
+    inc si
+    loop sort_j
+    pop cx          ; decrementam prima bucla
+    loop sort_i
 
-	;afisare cuvantul C
+    ; calculam numarul maxim de aparitii ale lui 1 si retinem primul octet unde se atinge acest maxim
+    mov max_bits, 0
+    lea si, valori
+    mov cl, nr_octeti
+    xor bx, bx        ; bl = pozitia curenta
+find_max:
+    mov al, [si]
+    call count_ones   ; Returneaza AH = nr de 1
+    cmp ah, max_bits
+    jbe next_elem     ; Daca AH <= max_bits, trecem la urmatorul
+    
+    mov max_bits, ah
+    mov val_max_bits, al
+    mov pos_max_bits, bl
+
+next_elem:
+    inc si
+    inc bl
+    loop find_max
+
+    ; rotiri cu N = suma primilor 2 biti
+    lea si, valori
+    lea di, valori_shift
+    mov cl, nr_octeti
+    xor ch, ch
+
+loop_octeti:
+    lodsb
+    mov bl, al
+    and bl, 03h       ; Verificam ultimii 2 biti
+    jz store_result   ; Daca sunt 00, nu rotim
+    
+    mov ah, 1         ; Presupunem 1 rotire
+    cmp bl, 3
+    jne rotate_start  ; Daca bl e 1 sau 2, facem 1 rotire
+    inc ah            ; Daca bl e 3, facem 2 rotiri
+
+rotate_start:
+    rol al, 1
+    dec ah
+    jnz rotate_start
+
+store_result:
+    stosb
+    loop loop_octeti
+
+    ; //////AFISARI:
+
+    ;afisare cuvantul C
     mov dx, offset msg2
     mov ah, 09h
     int 21h
@@ -101,18 +154,80 @@ sort_j:
     lea si, valori
     mov cl, nr_octeti
     xor ch, ch
-	afis_sort: 
+afis_sort: 
     lodsb 
     call print_bin8 
-    loop afis_sort
+    loop afis_sort 
 
+    ;afisare pozitia ocetului cu cei mai multi 1 si valoare acestuia
+    ;valoarea
+    mov dx, offset msg4
+    mov ah, 09h
+    int 21h
+    mov al, val_max_bits
+    call print_bin8
+
+    ;pozitia
+    mov dx, offset msg5
+    mov ah, 09h
+    int 21h
+    mov al, pos_max_bits
+    mov ah, 0
+    mov bl, 10
+    div bl           ; AL = zeci, AH = unități
+
+    add al, '0'
+    mov dl, al
+    mov ah, 02h
+    int 21h
+
+    add ah, '0'
+    mov dl, ah
+    mov ah, 02h
+    int 21h
+
+    ;afisare sirul de valori rotite
+    mov dx, offset msg6
+    mov ah, 09h
+    int 21h
+    lea si, valori_shift
+    mov cl, nr_octeti
+    xor ch, ch
+afis_rot_bin: 
+    lodsb
+    call print_bin8 
+    loop afis_rot_bin 
+
+    mov dx, offset msg7
+    mov ah, 09h
+    int 21h
+    lea si, valori_shift
+    mov cl, nr_octeti
+    xor ch, ch
+afis_rot_hex:
+    lodsb
+    mov bl, al
+    shr al, 4
+    call print_hex
+    mov al, bl
+    and al, 0Fh
+    call print_hex
+    mov dl, ' '
+    mov ah, 02h
+    int 21h
+    loop afis_rot_hex
 
 exit:
-     mov ax, 4C00h
-     int 21h
+    mov ax, 4C00h
+    int 21h
 
-;----------------------------------------------
+; =========================================================
+; PROCEDURI
+; =========================================================
 
+    ; Presupune SI = adresa sirului, CL = numar octeti
+    ; REZULTAT: AX = cuvantul de control C (AH=Suma, AL=Logice)
+    ; STARE REGISTRI: Păstrează BX, CX, DX, SI prin stivă. Modifică doar AX.
 CALCUL_C PROC
     push bx
     push cx
@@ -160,6 +275,26 @@ calc_l:
     ret
 CALCUL_C ENDP
 
+    ; Presupune AL = octetul de verificat
+    ; REZULTAT: AH = numărul de biți setați pe 1
+count_ones proc
+    push cx
+    push bx
+    xor ah, ah
+    mov bl, al
+    mov cx, 8
+c1: 
+    shl bl, 1
+    adc ah, 0
+    loop c1
+    pop bx
+    pop cx
+    ret
+count_ones endp
+
+    ; input_buffer completat de int 21h/0Ah
+    ; REZULTAT: CF=0 (succes, nr_octeti actualizat), CF=1 (eroare). 
+    ; Poate apela terminarea programului.
 VALIDARE_SI_EXIT proc
     mov al, input_buffer[1]
     cmp al, 4
@@ -235,11 +370,46 @@ CURATA_BUFFER proc
     mov input_buffer, 50
 
     pop di
-    pop cxs
+    pop cx
     pop ax
     ret
 CURATA_BUFFER endp
 
+print_bin8 proc
+    push cx
+    push bx
+    push ax
+    mov bl, al
+    mov cx, 8
+p8: 
+    shl bl, 1
+    mov dl, '0'
+    jnc o8
+    mov dl, '1'
+o8: 
+    mov ah, 02h
+    int 21h
+    loop p8
+    mov dl, ' '
+    mov ah, 02h
+    int 21h
+    pop ax
+    pop bx
+    pop cx
+    ret
+print_bin8 endp
+
+print_bin16 proc
+    push ax
+    mov al, ah
+    call print_bin8
+    pop ax
+    call print_bin8
+    ret
+print_bin16 endp
+
+    ; Presupune AL = caracter ASCII ('0'-'F')
+    ; REZULTAT: AL = valoare numerică (0-15)
 ascii_to_val proc
     cmp al, '9'
     jbe d1
@@ -249,3 +419,22 @@ d1:
     sub al, '0'
     ret
 ascii_to_val endp
+
+print_hex proc
+    push ax
+    push dx
+    cmp al, 9
+    jbe d2
+    add al, 7
+d2: 
+    add al, '0'
+    mov dl, al
+    mov ah, 02h
+    int 21h
+    pop dx
+    pop ax
+    ret
+print_hex endp
+
+code ends
+end start
